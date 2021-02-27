5f9e9251748ff1d565b0e01ce056df599466ea12
denisdenis9331 <denisdenis9331@gmail.com>
2021-02-27 23:53:15 +0500

    first commit

--- parser.js
+++ parser.js
@@ -1,27 +1,16 @@
-'use strict'
-const http = require('http')
-const htmlparser = require('htmlparser2')
-function ParseTitlePage () {
-    const parser = new htmlparser.Parser({
-        onopentag: (name, attr) => {
-            if (name == 'img') console.log(attr.src)
-            else console.log('not found')
-        }
+const cheerio = require('cheerio');
+const request = require('request');
 
-    }, {decodeEntities: false})
-    const options = {
-        hostname: 'www.e1.ru',
-        path: '/index.php',
-        method: 'GET',
-        headers: {
-            'user-agent': 'Mozilla/5.0'
-        }
-    }
-    http.get(options, (res) => {
-        res.on('data', (chunk) => parser.write(chunk))
-            .on('end', () => parser.end())
-    })
-        .on('error', (e) => console.error(e))
-}
-ParseTitlePage()
-module.exports = ParseTitlePage
+request({
+    method: 'GET',
+    url: 'https://www.google.com'
+}, (err, res, body) => {
+
+    if(err) return console.error(err);
+
+    let $ = cheerio.load(body);
+
+    let title = $('title');
+
+    console.log(title.text());
+});
